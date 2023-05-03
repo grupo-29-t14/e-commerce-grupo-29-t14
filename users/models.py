41ef765a4a5db30fd55768081ec37b7792101495
CToH10 <luisebn98@gmail.com>
2023-05-03 11:01:25 -0300

    feat: add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=120, unique=True)
+    is_seller = models.BooleanField(default=False)
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=120)
