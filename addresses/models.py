2fd47a1a3acfa92308a646ed9a36b410986ee9d5
CToH10 <luisebn98@gmail.com>
2023-05-03 11:50:33 -0300

    feat: add cart model

--- addresses/models.py
+++ addresses/models.py
@@ -4,7 +4,7 @@ import uuid
 
 class Address(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    user_id = models.ForeignKey(
+    user_id = models.OneToOneField(
         "users.User", on_delete=models.CASCADE, related_name="address"
     )
     city = models.CharField(max_length=50)
