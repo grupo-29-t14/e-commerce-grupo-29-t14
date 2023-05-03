51e90809501b33a14738541a9f447bf9abb866d2
CToH10 <luisebn98@gmail.com>
2023-05-03 11:05:02 -0300

    feat: add address model

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Address(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    user_id = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="address"
+    )
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    number = models.CharField(max_length=10)
