6028fc85341576af9975222f4a2ff32c8486dad5
CToH10 <luisebn98@gmail.com>
2023-05-04 11:19:59 -0300

    feat: add custom delete && update

--- _project/settings.py
+++ _project/settings.py
@@ -141,7 +141,4 @@ AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "rest_framework.permissions.IsAuthenticated",
-    # ],
 }
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from users.serializers import UserSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import User
@@ -9,7 +10,6 @@ class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     authentication_classes = [JWTAuthentication]
-    permission_classes = []
 
     def get_queryset(self):
         if self.request.user.is_superuser:
@@ -25,3 +25,22 @@ class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_url_kwarg = "pk"
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.is_active is False:
+            return Response(data={"message": "User already inactive"}, status=400)
+        self.perform_destroy(instance)
+        return Response(status=204)
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
+
+    def perform_update(self, serializer):
+        if self.request.user.is_superuser is False and serializer.validated_data.get(
+            "is_seller"
+        ):
+            serializer.validated_data.pop("is_seller", None)
+
+        return super().perform_update(serializer)
