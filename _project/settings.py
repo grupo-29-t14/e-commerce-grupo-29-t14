c28cc3e5a978113a7ff574c37e37b45f97543861
CToH10 <luisebn98@gmail.com>
2023-05-04 10:43:42 -0300

    feat: adminOrOwner permission

--- _project/settings.py
+++ _project/settings.py
@@ -141,4 +141,7 @@ AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    # "DEFAULT_PERMISSION_CLASSES": [
+    #     "rest_framework.permissions.IsAuthenticated",
+    # ],
 }
--- users/views.py
+++ users/views.py
@@ -2,16 +2,25 @@ from rest_framework import generics
 from users.serializers import UserSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import User
+from .permissions import IsAdminOrAccountOwner
 
 
 class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = []
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return User.objects.all()
+
+        return User.objects.filter(is_seller=True, is_active=True)
 
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
-    # permission_classes = []
+    permission_classes = [IsAdminOrAccountOwner]
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
